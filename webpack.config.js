6a2a3fa01dc587886fc350188d01df9ae6cbab59
Moroi <4635854+Rango-dz@users.noreply.github.com>
2022-04-01 15:10:12 +0100

    Uploading empty files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/assets'),
+    filename: 'bundle.js'
+  },
+  watch: true,
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
+    ]
+  }
+};
